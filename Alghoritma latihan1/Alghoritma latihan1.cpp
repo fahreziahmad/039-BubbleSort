1b971a2acb887f18da4ac6745e1adb8abd156efd
fahreziahmad <alfahreziahmadsyahyana@gmail.com>
2024-03-07 21:44:39 +0700

    display

--- Alghoritma latihan1/Alghoritma latihan1.cpp
+++ Alghoritma latihan1/Alghoritma latihan1.cpp
@@ -41,3 +41,20 @@ void bubbleSortArray() { // procedur untuk mengurutkan array dengan metode bubbl
 
 	} while (pass <= n - 1); //step 5
 }
+void display() {
+	cout << endl;
+	cout << "==================================" << endl;
+	cout << "Elemen Array yang telah tersusun" << endl;
+	cout << "==================================" << endl;
+	cout << endl;
+	for (int j = 0; j < n; j++) {
+		cout << arr[j];
+		if (j < n - 1) {
+			cout << " --> ";
+		}
+	}
+	cout << endl;
+	cout << endl;
+	cout << "Jumlah pass = " << n - 1 << endl;
+	cout << endl;
+}
